41c6ad8e6b82e20c8f8b770a4d8abc48a8ccbc96
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-08 10:57:14 +0530

    Configured prettier, format documents

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./booklist/templates/**/*.html'],
-    darkMode: 'selector',
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: ['./booklist/templates/**/*.html'],
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
