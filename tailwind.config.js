0624d8bec94f6e204575b73aa1f92580e126a028
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-08 10:55:17 +0530

    Configured tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./booklist/templates/**/*.html'],
+    darkMode: 'selector',
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
