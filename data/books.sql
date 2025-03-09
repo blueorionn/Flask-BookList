f9d48d494ca20d6678205b0fbde67d39e6b5f20b
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-09 10:03:51 +0530

    modified book data, corrected erros

--- data/books.sql
+++ data/books.sql
@@ -3,19 +3,21 @@ CREATE TABLE
         id VARCHAR(36) PRIMARY KEY,
         title VARCHAR(255) UNIQUE,
         summary TEXT,
-        ISBN INT UNSIGNED,
+        ISBN BIGINT UNSIGNED,
         genre VARCHAR(255),
         publication_year INT UNSIGNED,
         author VARCHAR(255),
         publisher VARCHAR(255) NULL,
         rating FLOAT
     )
+    
 INSERT INTO
     books (
         id,
         title,
         summary,
         ISBN,
+        genre,
         publication_year,
         author,
         publisher,
@@ -27,7 +29,8 @@ VALUES
         "Harry Potter and the Sorcerer's Stone",
         "Harry Potter's life is miserable. His parents are dead and he's stuck with his heartless relatives, who force him to live in a tiny closet under the stairs. But his fortune changes when he receives a letter that tells him the truth about himself: he's a wizard. A mysterious visitor rescues him from his relatives and takes him to his new home, Hogwarts School of Witchcraft and Wizardry.After a lifetime of bottling up his magical powers, Harry finally feels like a normal kid. But even within the Wizarding community, he is special. He is the boy who lived: the only person to have ever survived a killing curse inflicted by the evil Lord Voldemort, who launched a brutal takeover of the Wizarding world, only to vanish after failing to kill Harry.Though Harry's first year at Hogwarts is the best of his life, not everything is perfect. There is a dangerous secret object hidden within the castle walls, and Harry believes it's his responsibility to prevent it from falling into evil hands. But doing so will bring him into contact with forces more terrifying than he ever could have imagined.Full of sympathetic characters, wildly imaginative situations, and countless exciting details, the first installment in the series assembles an unforgettable magical world and sets the stage for many high-stakes adventures to come.",
         9780439554930,
-        "Fantasy, Magic" 1997,
+        "Fantasy, Magic",
+        1997,
         "J.K. Rowling",
         "Scholastic",
         4.4
@@ -35,7 +38,8 @@ VALUES
     (
         "26e3e8a8-a882-4a97-b06a-48c4b2f7eac6",
         "To Kill a Mockingbird",
-        "Set in the Deep South during the 1930s, this Pulitzer Prize-winning novel follows a young girl's experience of racial injustice in a small Alabama town. As she witnesses her father, a lawyer, defend a wrongly accused black man, she learns valuable lessons about prejudice, empathy, and understanding." 9780061120084,
+        "Set in the Deep South during the 1930s, this Pulitzer Prize-winning novel follows a young girl's experience of racial injustice in a small Alabama town. As she witnesses her father, a lawyer, defend a wrongly accused black man, she learns valuable lessons about prejudice, empathy, and understanding.",
+        9780061120084,
         "Fiction, Classics",
         1960,
         "Harper Lee",
@@ -44,13 +48,14 @@ VALUES
     ),
     (
         "b6a7c3b6-2398-49c1-a6c0-4ee7a5e7bfc0",
-        "1984",
+        "The Invisible Man",
         "Written in 1949, this dystopian novel depicts a totalitarian future society where the government exercises total control over its citizens. The story follows Winston Smith, a low-ranking member of the ruling Party, as he begins to question the official ideology and eventually rebels against the system, risking his life and sanity.",
-        9780451524935,
-        "Science Fiction, Dystopian",
-        1949,
-        "George Orwell" "Secker & Warburg",
-        4.6
+        9780486284728,
+        "Science Fiction, Classics",
+        1897,
+        "H.G. Wells",
+        "Pearson's Magazine",
+        4.1
     ),
     (
         "7bab5cff-6b86-4027-891e-b41441261b9e",
